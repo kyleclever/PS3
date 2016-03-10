b699fcef04f1fd3dd76823ebe1a9923d38e9fe53
Kyle <Kyle@128.4.222.66>
2016-03-10 17:14:46 -0500

    update test

--- PS2/src/test/java/base/AcountTestCase.java
+++ PS2/src/test/java/base/AcountTestCase.java
@@ -1,25 +1,30 @@
 package base;
 
+import org.junit.Before;
 import org.junit.Test;
 
-
 public class AcountTestCase {
-	Account test = new Account(1122, 20000);	
-
-	@Test
-	public void test() throws InsufficientFundsException{
+	Account test = new Account(1122, 20000);
+	
+	@Before
+	public void setup() {
 		test.setId(1122);
 		test.setBalance(20000.0);
-		test.setAnnuallnterestRate(0.045);
+		test.setAnnuallnterestRate(0.045);		
+	}
+
+	@Test
+	public void testcase() throws InsufficientFundsException {
 		test.withdraw(2500.0);
 		test.deposit(3000.0);
-		System.out.println("Your balance is: "+ test.getBalance()+ ", monthly interest: " + 
-				test.getMonthlyInterestRate()*100 + "% and the date of the account created: "+ test.getDateCreated());
+		System.out.println(
+				"Your balance is: " + test.getBalance() + ", monthly interest: " + test.getMonthlyInterestRate() * 100
+						+ "% and the date of the account created: " + test.getDateCreated());
 	}
 	// Exception Test
 	@Test(expected = InsufficientFundsException.class)
-	public void OverDraw() throws InsufficientFundsException{
-		test.withdraw(50000.0);	
-		
+	public void OverDraw() throws InsufficientFundsException {
+		test.withdraw(50000.0);
+
 	}
 }
